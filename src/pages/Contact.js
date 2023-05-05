e4029f9c870cd54b10ce4292f99a2c602b8ce3b6
geostojanov <gestojanov@gmail.com>
2023-05-05 11:50:45 +0200

    wrap

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,8 @@ export default function Contact() {
   return (
     <div>
       <h2>Contact</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quas commodi, reiciendis similique, minus animi earum molestiae odit recusandae incidunt suscipit deleniti ullam! Nulla, sequi. Officia reprehenderit accusantium ex magni?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quas commodi, reiciendis similique, minus animi earum
+        molestiae odit recusandae incidunt suscipit deleniti ullam! Nulla, sequi. Officia reprehenderit accusantium ex magni?</p>
     </div>
   )
 }

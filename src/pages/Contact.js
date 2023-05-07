3c25a0882c0fd26f593bba9461fa55210cd972d5
geostojanov <gestojanov@gmail.com>
2023-05-07 15:26:05 +0200

    Query parameters

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 export default function Contact() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name")
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quas commodi, reiciendis similique, minus animi earum
         molestiae odit recusandae incidunt suscipit deleniti ullam! Nulla, sequi. Officia reprehenderit accusantium ex magni?</p>
     </div>
